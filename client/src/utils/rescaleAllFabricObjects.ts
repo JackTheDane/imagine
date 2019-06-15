6723eda27420f65f0b8687efe4e5a4849aa8fb05
Martin Petersen <Fader4ever@hotmail.com>
2019-06-15 21:18:55 +0200

    Fixed group scaling during resizing

--- client/src/utils/rescaleAllFabricObjects.ts
+++ client/src/utils/rescaleAllFabricObjects.ts
@@ -11,9 +11,42 @@ export const rescaleAllFabricObjects = (
   canvas.setHeight(canvas.getHeight() * newScale);
 
   const objects: fabric.Object[] = canvas.getObjects();
+  console.log(objects);
+
+  // array of already scaled groups to prevent groups being scaled multiple times
+  const alreadyScaledGroups: fabric.Group[] = [];
 
   for (let i = 0; i < objects.length; i++) {
     const object = objects[i];
+
+
+    if (object.group) {
+
+      // Continue if the group has already been scaled
+      if (alreadyScaledGroups.includes(object.group)) {
+        continue;
+      }
+
+      const scale: number = object.group.scaleX
+        ? object.group.scaleX
+        : object.group.scaleY
+          ? object.group.scaleY
+          : 1;
+
+      object.group.scale(scale * newScale);
+
+      if (object.group.top) {
+        object.group.top = object.group.top * newScale;
+      }
+
+      if (object.group.left) {
+        object.group.left = object.group.left * newScale;
+      }
+
+      alreadyScaledGroups.push(object.group);
+
+      continue;
+    }
 
     const scale: number = object.scaleX
       ? object.scaleX
