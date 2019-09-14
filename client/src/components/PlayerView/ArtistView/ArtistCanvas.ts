12f22e15caa4621b1dd6346d5b136d00baa3ab2e
jackthedane <fader4ever@hotmail.com>
2019-09-14 12:58:56 +0200

    WIP - Moving rest of functions over

--- client/src/components/PlayerView/ArtistView/ArtistCanvas.ts
+++ client/src/components/PlayerView/ArtistView/ArtistCanvas.ts
@@ -116,9 +116,404 @@ export class ArtistCanvas {
 
   // Rescale all Figs
 
+  // ----- Transfered functions --> Consider moving to own "Utilities" folder for better overview
+
   private getValueToHeightScale = (value: number): number => value / getCanvasHeightFromWidth(this.canvasWidth || 1);
   private getValueToWidthScale = (value: number): number => value / (this.canvasWidth || 1);
   private getScaledScale = (scaleValue: number): number => Math.round(this.getValueToWidthScale(scaleValue) * scaleFactor);
+
+  /**
+	 * Returns an ISavedFabricObject from a fabric.Object.
+	 * This includes the coordinates of any group that the object might be part of.
+	 *
+	 * @param fabric.Object
+	 */
+  private getSavedFabricObjectFromObject = (object: fabric.Object): ISavedFabricObject | undefined => {
+    if (!this.c || !object) {
+      return;
+    }
+
+    const { top, left, scaleX, scaleY, angle, group } = object;
+
+    const r: ISavedFabricObject = {
+      top: getValueElse(top, 0),
+      scale: getValueElse(scaleX || scaleY, 0),
+      left: getValueElse(left, 0),
+      angle: getValueElse(angle, 0),
+      src: (object as fabric.Image).getSrc()
+    };
+
+    if (group) {
+
+      if (group.angle) {
+        r.angle += group.angle;
+
+        if (object.top != null && object.left != null) {
+          const [x, y]: [number, number] = getThirdPointInTriangle(0, 0, object.left, object.top, group.angle);
+
+          r.left = x;
+          r.top = y;
+        }
+      }
+
+      const scalingFactor: number = group.scaleX || group.scaleY || 1;
+
+      if (scalingFactor !== 1) {
+        r.scale *= scalingFactor;
+        r.top *= scalingFactor;
+        r.left *= scalingFactor;
+      }
+
+      if (group.top != null) {
+        r.top += group.top;
+      }
+
+      if (group.left != null) {
+        r.left += group.left;
+      }
+    }
+
+    return {
+      top: Math.round(r.top),
+      angle: Math.round(r.angle),
+      left: Math.round(r.left),
+      scale: r.scale,
+      src: r.src
+    };
+  };
+
+  private addToStoredObjectEvents = (objectName: string, newEvent: IObjectEvent) => {
+    const events: IObjectEvent[] | undefined = this.storedObjectEvents[objectName];
+    this.storedObjectEvents[objectName] = events && events.length > 0
+      ? [
+        ...events,
+        newEvent
+      ]
+      : [newEvent];
+  }
+
+
+  private getObjectSnapshotByIndex = (index: number): IObjectSnapshot | undefined => {
+    const {
+      historyIndex,
+      snapshotHistory
+    } = this.state;
+
+    // If the index is too low, is the same as the current index or is greater that the current length - 1, return
+    if (
+      index < 0
+      || index > snapshotHistory.length - 1
+      || index === historyIndex
+    ) {
+      return;
+    }
+
+    // Get the snapshot of the for the given index
+    return snapshotHistory[index];
+  }
+
+	/**
+	 * Sets the Canvas object state based on snapshotHistory index
+	 *
+	 * @param index: number
+	 */
+  private setCanvasObjectStateByHistoryIndex = (index: number): void => {
+
+    const snapshot: IObjectSnapshot | undefined = this.getObjectSnapshotByIndex(index);
+
+    if (!snapshot) {
+      return;
+    }
+
+    this.setObjectsFromSnapshot(snapshot);
+    this.setState({
+      historyIndex: index
+    });
+  }
+
+	/**
+	 * Sets the Canvas Objects state based on a snapshot
+	 *
+	 * @param IObjectSnapshot
+	 */
+  private setObjectsFromSnapshot = (snapshot: IObjectSnapshot): void => {
+    if (!this.c || !snapshot) {
+      return;
+    }
+
+    if (this.c.getActiveObject) {
+      this.c.discardActiveObject();
+    }
+
+    // Get all Canvas Objects
+    const objects: fabric.Object[] = this.c.getObjects('image');
+
+
+    // These are names that where not found on the canvas
+    let notYetUsedSnapshots: string[] = Object.keys(snapshot);
+
+    const usedObject: fabric.Object[] = [];
+
+    const addToUsedObjects = (object: fabric.Object) => {
+      if (!usedObject.includes(object)) {
+        usedObject.push(object);
+      }
+    }
+
+    for (let i = 0; i < objects.length; i++) {
+      const o = objects[i];
+
+      if (!o.name) {
+        this.c.remove(o);
+        continue;
+      }
+
+      const oSnapshot: ISavedFabricObject | undefined = snapshot[o.name];
+
+      // If the object does not exist in the snapshot
+      if (!oSnapshot) {
+        // If not found in the snapshot, remove it and continue
+        this.c.remove(o);
+        continue;
+      }
+
+      // If the a matching key was found between the two snapshots, remove it from "notYetUsedSnapshots"
+      notYetUsedSnapshots = notYetUsedSnapshots.filter((name: string): boolean => name !== o.name);
+
+      // Check all of the properties for differences, and if any are found, set the object values to those of the snapshot
+      const checkAndSetProperty = (property: 'left' | 'top' | 'angle'): void => {
+        if (o[property] !== oSnapshot[property]) {
+          o.set(property, oSnapshot[property]);
+          addToUsedObjects(o);
+        }
+      }
+
+      checkAndSetProperty('left');
+      checkAndSetProperty('top');
+      checkAndSetProperty('angle');
+
+      if (o.scaleX !== oSnapshot.scale) {
+        o.set('scaleX', oSnapshot.scale);
+        o.set('scaleY', oSnapshot.scale);
+        addToUsedObjects(o);
+      }
+    };
+
+    // Run through each
+    notYetUsedSnapshots.forEach((name: string) => {
+
+      const {
+        scale,
+        src,
+        ...rest
+      } = snapshot[name];
+
+      this.addImageToCanvas(
+        src, {
+          ...rest,
+          name,
+          scaleX: scale,
+          scaleY: scale
+        }
+      );
+    });
+
+    if (usedObject.length > 0) {
+      const sel = new fabric.ActiveSelection(usedObject, {
+        canvas: this.c
+      });
+
+      this.c.setActiveObject(sel).requestRenderAll();
+    }
+  }
+
+  private generateSnapshotsFromObjects = (objects: fabric.Object[]): IObjectSnapshot => {
+
+    const objectSnapshot: IObjectSnapshot = {};
+
+    objects.forEach(o => {
+      // Check for object name and that it is not already set to the snapShot
+      if (o.name && !objectSnapshot[o.name]) {
+        const savedFabricObject: ISavedFabricObject | undefined = this.getSavedFabricObjectFromObject(o);
+
+        // Check if savedFabricObject was returned correctly
+        if (savedFabricObject) {
+          objectSnapshot[o.name] = savedFabricObject; // If so, set to snapshot
+        }
+      }
+    });
+
+    return objectSnapshot;
+  };
+
+  // -- Canvas - Adders -- //
+
+  private addNewImageToCanvas = (src: string, options?: fabric.IImageOptions): void => {
+
+    this.addImageToCanvas(src, options)
+      .then(img => {
+        if (!img.name) {
+          return;
+        }
+
+        const savedFabricObject: ISavedFabricObject | undefined = this.getSavedFabricObjectFromObject(img);
+
+        if (!savedFabricObject) {
+          return;
+        }
+
+        // Get current snapshot and add the new image to it
+        const snapshot: IObjectSnapshot = {
+          ...this.objectsSnapshot,
+          [img.name]: savedFabricObject
+        };
+
+        // Add new snapshot to snapshot history
+        this.addToSnapshotToHistory(snapshot);
+      })
+  }
+
+  private addImageToCanvas = (src: string, options?: fabric.IImageOptions): Promise<fabric.Image> => {
+    return new Promise((resolve, reject) => {
+      if (!this.c) {
+        reject();
+        return;
+      }
+
+      const imgOptions: fabric.IImageOptions = { ...options };
+
+      if (!options || !options.name) {
+        imgOptions.name = 'o' + this.objectIndex++;
+      }
+
+      fabric.Image.fromURL(
+        src,
+        (img: fabric.Image) => {
+          if (this.c) {
+
+            // Get the smallest axis of the image
+            const smallestAxis: number = (img.width as number) >= (img.height as number) ? (img.width as number) : (img.height as number);
+
+            // This ensures that an image cannot scale to be smaller than 80px on its smallest scale (Height or width)
+            img.minScaleLimit = 120 / smallestAxis;
+
+            if (img.minScaleLimit > 1) {
+              img.scaleX = img.minScaleLimit;
+              img.scaleY = img.minScaleLimit;
+            }
+
+            img.scaleToWidth(200);
+
+            if (!img.top || !img.left) {
+              img.top = this.c.getHeight() / 2;
+              img.left = this.c.getWidth() / 2;
+            }
+
+            this.c.add(img);
+
+            this.c.setActiveObject(img);
+
+            resolve(img);
+          }
+        },
+        imgOptions
+      );
+    });
+  };
+
+  private getObjectChangesFromSnapshot = (snapshot: IObjectSnapshot): IObjectChanges | false => {
+    if (!this.c || !this.objectsSnapshot || !snapshot) {
+      return false;
+    }
+
+    const changes: IObjectChanges = {};
+
+    // Loop through all of the keys of the old snapshot
+    Object.keys(this.objectsSnapshot).forEach((name: string) => {
+      const newObject: ISavedFabricObject = snapshot[name];
+
+      // If an equivalent object does not exist on the new snapshot, return
+      if (!newObject) {
+        return;
+      }
+
+      const storedObjectChanges: IObjectEvent[] = this.storedObjectEvents[name];
+      const oldObject: ISavedFabricObject = this.objectsSnapshot[name];
+      const changesArray: IObjectEvent[] = storedObjectChanges ? [...storedObjectChanges] : [];
+
+      // Check for changes in left coordinaties
+      if (newObject.left !== oldObject.left) {
+        changesArray.push({
+          type: ObjectEventTypes.left,
+          data: this.getValueToWidthScale(newObject.left)
+        });
+      }
+
+      if (newObject.top !== oldObject.top) {
+        changesArray.push({
+          type: ObjectEventTypes.top,
+          data: this.getValueToHeightScale(newObject.top)
+        });
+      }
+
+      // Check for changes in angle
+      if (newObject.angle !== oldObject.angle) {
+
+        changesArray.push({
+          type: ObjectEventTypes.angle,
+          data: newObject.angle
+        });
+      }
+
+      // Check for changes in scale
+      if (newObject.scale !== oldObject.scale) {
+        changesArray.push({
+          type: ObjectEventTypes.scale,
+          data: this.getScaledScale(newObject.scale)
+        });
+      }
+
+      if (changesArray.length > 0) {
+        changes[name] = changesArray;
+      }
+    });
+
+    // Reset storedObjectEvents
+    this.storedObjectEvents = {};
+
+    return Object.keys(changes).length > 0 && changes;
+  };
+
+  // Socket.io interactions
+  private sendCanvasChange = (event: IGameEvent) => {
+    if (!this.props.ioSocket) {
+      return;
+    }
+
+    this.props.ioSocket.emit("cEvent", JSON.stringify(event));
+  }
+
+  private getScaledImageInfo = (image: fabric.Image): IImageInfo | undefined => {
+
+    const savedFabricObject: ISavedFabricObject | undefined = this.getSavedFabricObjectFromObject(image);
+
+    if (!savedFabricObject || image.name == null) {
+      return;
+    }
+
+    const {
+      left,
+      top,
+      ...rest
+    } = savedFabricObject;
+
+    return ({
+      ...rest,
+      top: this.getValueToHeightScale(top),
+      left: this.getValueToWidthScale(left),
+      name: image.name
+    });
+  }
 
 
   private _addEventListeners = () => {
